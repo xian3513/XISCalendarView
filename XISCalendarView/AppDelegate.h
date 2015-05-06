03a13415928ca50de3d2667e0c971e34f16276aa
kt <kt@a2ndeiMac.local>
2015-05-06 16:02:39 +0800

    Initial Commit

---
+++ XISCalendarView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XISCalendarView
+//
+//  Created by kt on 15/5/6.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XISCalendarView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XISCalendarView
+//
+//  Created by kt on 15/5/6.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
